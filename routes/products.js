7ec475379180d99d2662b6893fade121cbdd9900
altunbas.huseyin@gmail.com <Huso7474>
2014-09-26 16:20:48 +0300

    Son Güncellemeler

---
+++ routes/products.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+    mongoose.model("products").find(function(err,users)
+    {
+        res.send(users);
+    });
+
+});
+
+module.exports = router;
